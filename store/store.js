caaf69fca280eeca67ff74bf05e3365a4913f29f
daniel <daniel.evanenko@gmail.com>
2025-02-19 21:20:35 +0200

    init repo

---
+++ store/store.js
@@ -0,0 +1,10 @@
+
+const { createStore, combineReducers, compose } = Redux
+
+const rootReducer = combineReducers({
+    carModule: carReducer,
+    userModule: userReducer
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+export const store = createStore(rootReducer, composeEnhancers())
